03d4489c21f848529913dba9e2f6366e87a7d646
brandiw <brandimichelle.0911@gmail.com>
2017-05-09 15:56:21 -0700

    Updating components and factory

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-angular.module('CardsAgainstAssembly', ['CardCtrls']);
+angular.module('CardsAgainstAssembly', ['CardsApp'])
+.controller("CardsCtrl", ["$scope", "QuestionsFactory", function($scope, QuestionsFactory){
+  $scope.qCards = QuestionsFactory.getCards();
+  $scope.displayCard = $scope.qCards[pickCardIndex($scope.qCards.length)];
+  $scope.numPlayers = 3;
+  $scope.errorMessage = "";
+
+  $scope.$watch("numPlayers", function(newVal, oldVal){
+    $scope.errorMessage = "";
+
+    if(newVal <= 0){
+      $scope.errorMessage = "It really isn't that fun to play by yourself. :(";
+    }
+    else if(newVal > 10){
+      $scope.errorMessage = "That seems like too many people.";
+    }
+  });
+}]);
+
+function pickCardIndex(size){
+  return Math.floor(Math.random() * size);
+}
---
+++ js/components/answerCards.js
@@ -0,0 +1,29 @@
+angular.module("CardsAgainstAssembly")
+.component("answerCards", {
+  templateUrl: "js/components/views/answerCards.html",
+  controller: ["$scope", "AnswersFactory", AnswerCardsController],
+  controllerAs: "ac",
+  scope: true
+});
+
+function AnswerCardsController($scope, AnswersFactory){
+  var ac = this;
+  var cards = AnswersFactory.getCards();
+  ac.aCards = shuffleArray(cards, $scope.$parent.numPlayers);
+
+  $scope.$parent.reset = function(num){
+    if(num >= 0 && num <= 10){
+      ac.aCards = shuffleArray(cards, num);
+    }
+  }
+}
+
+function shuffleArray(arr, limit){
+  if(limit > arr.length){
+    limit = arr.length;
+  }
+  var shuffled = arr.sort(function(){
+    return 0.5 - Math.random(); //not actually that random, but whatevs
+  });
+  return shuffled.slice(0, limit);
+}
