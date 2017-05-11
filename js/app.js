913848cf304b4cccab9e59f3781e151665da495c
alainabuzas <alaina.buzas@gmail.com>
2017-05-11 16:23:39 -0700

    set up multiple decks within answers factory

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 angular.module('CardsAgainstAssembly', ['CardsApp'])
-.controller("CardsCtrl", ["$scope", "QuestionsFactory", function($scope, QuestionsFactory){
-  console.log("Cards Controller");
-}]);
+    .controller("CardsCtrl", ["$scope", "QuestionsFactory", function($scope, QuestionsFactory) {
+        console.log("Cards Controller");
 
+        var questionCards = QuestionsFactory.getCards();
+        $scope.numPlayers = 3;
+
+        $scope.newQuestion = function() {
+            $scope.displayCard = questionCards[pickCardIndex(questionCards.length)].question;
+        }
+
+        $scope.newQuestion();
+
+    }]);
+
+function pickCardIndex(deckSize) {
+    return Math.floor(Math.random() * deckSize);
+}
--- js/components/answerCards.js
+++ js/components/answerCards.js
@@ -1,4 +1,33 @@
 angular.module("CardsAgainstAssembly")
-.component("answerCards", {
-  //Stuff goes here
-});
+    .component("answerCards", {
+        templateUrl: "js/components/views/answerCards.html",
+        controller: ["$scope", "AnswersFactory", AnswerCardsController],
+        controllerAs: "ac",
+        scope: true
+    });
+
+function AnswerCardsController($scope, AnswersFactory) {
+    var ac = this;
+    var deckset =
+
+        if (deckset === webdev) {
+            var cards = AnswersFactory.getDevCards()
+        } else {
+            var cards = AnswersFactory.getCards()
+        };
+
+    ac.answerCards = shuffleAndLimit(cards, $scope.$parent.numPlayers)
+
+}
+
+function shuffleAndLimit(cards, limit) {
+    if (limit > cards.length) {
+        limit = cards.length;
+    }
+
+    var shuffled = cards.sort(function() {
+        return 0.5 - Math.random();
+    });
+
+    return shuffled.slice(0, limit);
+}
